5db1e94489cd12db09434ec14245a51d190035ee
TobyHv <andresjosue@hotmail.es>
2025-03-24 20:04:32 -0600

    helmet

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const app = require('./app');
+const helmet = require('helmet')
+
+app.use(helmet());
 
 app.listen(app.get('port'), () =>{
     console.log("Servidor en el puerto", app.get("port"));
